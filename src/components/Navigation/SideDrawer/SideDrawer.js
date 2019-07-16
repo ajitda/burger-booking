f3e7ff7b7385cf2603f5c3ccdd0d45d868ddd27f
ajit7 <ajitdas2900@gmail.com>
2019-07-16 19:40:21 +0600

    sidedrawer added

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../../Navigation/NavigationItems/NavigationItems';
+import './SideDrawer.css';
+
+const sideDrawer = () => {
+    return (
+        <div className="SideDrawer">
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+    );
+}
+
+export default sideDrawer;
