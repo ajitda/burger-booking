73157c38a0b0856194507d93355d7ad49c79b0d6
ajitda <ajitdas2900@gmail.com>
2019-09-17 00:10:44 +0600

    dynamic form element added

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -82,12 +82,25 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementArray = [];
+        for (let key in this.state.orderForm) {
+            formElementArray.push({
+                id:key,
+                config:this.state.orderForm[key]
+            })
+        }
         let form = (
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-                <Input inputtype="input" type="text" name="stree" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postalCode" placeholder="Postal Code" />
+                {
+                    formElementArray.map(formElement=>(
+                        <Input 
+                            key={formElement.id}
+                            elementType = {formElement.config.elementType} 
+                            elementConfig = {formElement.config.elementConfig} 
+                            value = {formElement.config.value} 
+                        />
+                    ))
+                }
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
